94460f9f4e2c39741f0f254168bfa8d1cff865a9
jhekasoft <jheka@mail.ru>
2014-12-13 03:50:02 +0200

    Blinking

--- gpio01.py
+++ gpio01.py
@@ -8,11 +8,16 @@ GPIO.setmode(GPIO.BOARD)
 led1 = 8 #7 #23
 led2 = 11
 led3 = 12
+led4 = 13
+led5 = 15
 button = 5
+ledstate = 1
 
 GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 GPIO.setup(led3, GPIO.OUT)
+GPIO.setup(led4, GPIO.OUT)
+GPIO.setup(led5, GPIO.OUT)
 GPIO.setup(button, GPIO.IN)
 
 #GPIO.output(led1, 0)
@@ -20,11 +25,13 @@ GPIO.setup(button, GPIO.IN)
 #GPIO.output(led3, 0)
 #sys.exit()
 
-GPIO.output(led1, 1)
-GPIO.output(led2, 1)
-GPIO.output(led3, 1)
+GPIO.output(led1, ledstate)
+GPIO.output(led2, ledstate)
+GPIO.output(led3, ledstate)
+GPIO.output(led4, ledstate)
+GPIO.output(led5, ledstate)
 
-time.sleep(1)
+#time.sleep(1)
 #time.sleep(random.uniform(5,10))
 #GPIO.output(led, 0)
 
@@ -32,14 +39,25 @@ time.sleep(1)
 #    print ("%s", str(GPIO.input(button)))
 #    time.sleep(0.01)
 
-while GPIO.input(button):
-    pass
+while 1:
+    time.sleep(1)
+    ledstate = 1 - ledstate
+    print("state: %d" % ledstate)
+    GPIO.output(led1, ledstate)
+    GPIO.output(led2, ledstate)
+    GPIO.output(led3, ledstate)
+    GPIO.output(led4, ledstate)
+    GPIO.output(led5, ledstate)
 
-if GPIO.input(button) == False:
-    GPIO.output(led1, 0)
-    GPIO.output(led2, 0)
-    GPIO.output(led3, 0)
-    print("Button pressed")
+    # while GPIO.input(button):
+    #     pass
+
+    # if GPIO.input(button) == False:
+    #     GPIO.output(led1, 0)
+    #     GPIO.output(led2, 0)
+    #     GPIO.output(led3, 0)
+    #     GPIO.output(led4, 0)
+    #     GPIO.output(led5, 0)
+    #     print("Button pressed")
 
 GPIO.cleanup()
-
