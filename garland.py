8633c217143872408d9721d0dd7cd5f76c0518c6
jhekasoft <jheka@mail.ru>
2014-12-13 22:00:00 +0200

    Keyboard interrupt

--- garland.py
+++ garland.py
@@ -3,25 +3,32 @@ import time
 import random
 import sys
 
-GPIO.setmode(GPIO.BOARD)
-leds = [8, 11, 12, 13, 15, 16]
-button = 5
-ledstate = 1
-
-GPIO.setup(button, GPIO.IN)
-for led in leds:
-    GPIO.setup(led, GPIO.OUT)
-
-while GPIO.input(button):
-    time.sleep(0.1)
-    ledstate = 1 - ledstate
-    print("state: %d" % ledstate)
+def gpio_cleanup(leds):
     for led in leds:
-        GPIO.output(led, ledstate)
+        GPIO.output(led, 0)
+    GPIO.cleanup()
 
-if GPIO.input(button) == False:
-    print("Button pressed")
+try:
+    GPIO.setmode(GPIO.BOARD)
+    leds = [8, 11, 12, 13, 15, 16]
+    button = 5
+    ledstate = 1
 
-for led in leds:
-    GPIO.output(led, 0)
-GPIO.cleanup()
+    GPIO.setup(button, GPIO.IN)
+    for led in leds:
+        GPIO.setup(led, GPIO.OUT)
+
+    while GPIO.input(button):
+        print("state: %d" % ledstate)
+        for led in leds:
+            GPIO.output(led, ledstate)
+        time.sleep(1)
+        ledstate = 1 - ledstate
+
+    if GPIO.input(button) == False:
+        print("Button pressed")
+
+    gpio_cleanup(leds)
+
+except KeyboardInterrupt:
+    gpio_cleanup(leds)
